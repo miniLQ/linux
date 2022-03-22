8e4b8f0fe6d6254c0154942cbe082c603cd42c65
rlk <luteresa@163.com>
2022-03-23 00:03:14 +0800

    add comments for fixmap

--- my_notes/readmm.c
+++ my_notes/readmm.c
@@ -38,29 +38,47 @@ mem_map，与物理页面一一对
 由pfn可以找到page
 __pfn_to_page(pfn)
 
+分配物理页面的核心接口函数：
+<include/linux/gfp.h>
 
-伙伴系统：
-迁移类型，解决碎片化问题
-	MIGRATE_UNMOVABLE :在内存中位置固定，不能随意移动，比如内核分配的内存
-	MIGRATE_MOVABLE：可以随意移动，用户态app分配的内存；
-	MIGRATE_RECLAIMABLE： 不能移动，但可以回收的内存，比如文件映射缓存；
-在伙伴系统的空闲链表中，每个链表都根据移动类型划分多个链表；
+/********************************************************************
+ * func:分配2的order次幂个连续的物理页面
+ * gfp_mask: 分配掩码
+ * order: oder应该小于MAX_ORDER(默认11)
+ *
+ * 返回值：第一个物理页面的page数据结构
+ ********************************************************************/
+struct page *alloc_pages(gfp_t gfp, unsigned int order)
 
-迁移类型的最小单位是pageblock, 2~order
+<mm/page_alloc.c>
+unsigned long __get_free_pages(gfp_t gfp_mask, unsigned int order)
 
-set_pageblock_migratetype(struct page * page, int migratetype)
-get_pageblock_migratetype(page)
+分配一个一个物理页面
+alloc_page(gfp_mask)
+__get_free_page(gfp_mask)
+返回全零页面
+get_zeroed_page(gfp_t gfp_mask)
+=========
 
-两个块大小相同；
-两个块地址连续；
-两个块必须是同一个大块分离出来的；
+释放页面
+void __free_pages(struct *page, unsigned int order);
 
-页面分配器是基于zone设计的，
-系统会优先使用ZONE_HIGHMEM, 然后才是ZONE_NORMAL;
-从分配掩码中，知道从那个zone分配内存：
-	for_each_zone_zonelist_nodemask(zone, z, zlist, highidx, nodemask)
-从分配掩码中，分配器知道从哪个迁移类型中分配内存；
-gfpflags_to_migratetype
+#define __free_page(page) __free_pages((page),0)
+#define free_page(addr) free_pages((addr),0)
+========================================================================================
+/********************************************************************
+ * func:分配2的order次幂个连续的物理页面
+ * gfp_mask: 分配掩码
+ * order: oder应该小于MAX_ORDER(默认11)
+ *
+ * 返回值：第一个物理页面的page数据结构
+ ********************************************************************/
+struct page *alloc_pages(gfp_t gfp, unsigned int order)
+__alloc_pages_nodemask(
+
+
+
+
 
 
 slab:
@@ -76,4 +94,36 @@ kmalloc函数
 
 void *kmalloc(size_t size, gfp_t flags)
 void kfree(const void *)
+
+
+
+
+	alloc_pages
+	
+两个问题：
+1.新创建一个slab描述符时，实际上没有分配物理页面；只有调用kmem_cache_alloc()时才真正从伙伴系统申请物理页面？
+那刚开始申请缓冲对象都会比较慢，申请次数多了之后，反而会更快(有空闲对象，直接获得)，这样理解正确吗？
+
+2.创建一个新的slab分配器时，返回的是首页page；而page本身是否也是用slab实现的？如果是，这里是不是有个嵌套，鸡生蛋，还是蛋生机鸡问题？
+
+还有个疑问
+问题3：slab描述符的成员，slab节点，struct kmem_cache_node *node[MAX_NUMNODES]; ///slab节点，在NUMA系统中，每个节点有一个kmem_cache_node数据结构
+
+vmalloc:
+适合分配较大内存；
+可能睡眠，不能用在中断上下文；
+--------------------------------------
+
+| |||||
+------------------------------------------
+
+
+start_kernel(void)
+setup_arch  
+early_fixmap_init
+
+
+
+
+early_ioremap(resource_size_t phys_addr, unsigned long size)
 
