d873012084f32065857fd0ca94539f935fbafbc6
yuyunbo <yuyunbo519@gmail.com>
2022-08-25 09:28:42 +0000

    add test for ktop

--- k_shared/test.c
+++ k_shared/test.c
@@ -3,14 +3,14 @@
 #include <string.h>
 #include <time.h>
 
-int main()
+int main(int argc, char **argv)
 { 
-	printf("hello\n");
 	int count = 0;
 	
-	
+	int val = atoi(argv[1]);
+	printf("hello,input:%d\n",val);
 	while(1) {
-		usleep(1*1000);
+		//usleep(val*1000);
 		count++;
 	}
 
