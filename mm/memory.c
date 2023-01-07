5bacb192869674df25da27f03f7d8fcfc75b0074
yuyunbo <yuyunbo519@gmail.com>
2023-01-07 09:39:27 +0000

    fixed error

--- mm/memory.c
+++ mm/memory.c
@@ -3330,6 +3330,8 @@ copy:
 
 static void unmap_mapping_range_vma(struct vm_area_struct *vma,
 		unsigned long start_addr, unsigned long end_addr,
+		struct zap_details *details)
+{
 	zap_page_range_single(vma, start_addr, end_addr - start_addr, details);
 }
 
